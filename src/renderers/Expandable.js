61dc2fdd45981fc53bc83f98a693a1c4d878a19d
Florante Dela Cruz <fdelacruz@dnanexus.com>
2018-11-09 19:21:21 -0800

    Use any element for Expandable 'icons'
    - Removing requirement of using 'mi' class based icons

--- src/renderers/Expandable.js
+++ src/renderers/Expandable.js
@@ -3,31 +3,31 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import {submitEvent} from '../eventWrappers';
-import {getNodeRenderOptions, updateNode} from '../selectors/nodes';
+import {updateNode} from '../selectors/nodes';
 import {Renderer} from '../shapes/rendererShapes';
 
 const Expandable = ({
   onChange,
-  node,
+  node = {},
   children,
-  iconsClassNameMap = {
-    expanded: 'mi mi-keyboard-arrow-down',
-    collapsed: 'mi mi-keyboard-arrow-right',
-    lastChild: '',
+  iconsElementMap = {
+    expanded: <span>-</span>,
+    collapsed: <span>+</span>,
+    lastChild: <span>*</span>
   },
 }) => {
-  const {hasChildren, isExpanded} = getNodeRenderOptions(node);
-  const className = classNames({
-    [iconsClassNameMap.expanded]: hasChildren && isExpanded,
-    [iconsClassNameMap.collapsed]: hasChildren && !isExpanded,
-    [iconsClassNameMap.lastChild]: !hasChildren,
-  });
-
+  const {expanded, collapsed, lastChild} = iconsElementMap;
+  const {state = {}, children: nodeChildren = []} = node;
+  const {expanded: isExpanded = false} = state;
+  const hasChildren = nodeChildren.length > 0;
+  const iconElement = !hasChildren ? lastChild : isExpanded ? expanded : collapsed;
   const handleChange = () => onChange(updateNode(node, {expanded: !isExpanded}));
 
   return (
     <span onDoubleClick={handleChange}>
-      <i tabIndex={0} onKeyDown={submitEvent(handleChange)} onClick={handleChange} className={className} />
+      <span tabIndex={0} onKeyDown={submitEvent(handleChange)} onClick={handleChange}>
+        {iconElement}
+      </span>
       {children}
     </span>
   );
@@ -36,10 +36,10 @@ const Expandable = ({
 Expandable.propTypes = {
   ...Renderer,
   iconsClassNameMap: PropTypes.shape({
-    expanded: PropTypes.string,
-    collapsed: PropTypes.string,
-    lastChild: PropTypes.string,
-  }),
+    expanded: PropTypes.element,
+    collapsed: PropTypes.element,
+    lastChild: PropTypes.element,
+  })
 };
 
 export default Expandable;
