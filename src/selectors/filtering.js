cac77a9d2d9cca19b993a09649901b685cfa99d5
Florante Dela Cruz <fdelacruz@dnanexus.com>
2018-11-12 12:29:56 -0800

    Allow setting of node.state on node filtering
    - Use case: Render all filtered nodes as initially expanded by setting
    stateOverride = {state.expanded = true}

--- src/selectors/filtering.js
+++ src/selectors/filtering.js
@@ -1,9 +1,9 @@
 const INITIAL_FILTERED_VALUE = {nodes: [], nodeParentMappings: {}};
 
-export const filterNodes = (filter, nodes, parents = []) =>
+export const filterNodes = (filter, nodes, parents = [], stateOverride = {}) =>
   nodes.reduce((filtered, n) => {
     const {nodes: filteredChildren, nodeParentMappings: childrenNodeMappings} = n.children
-      ? filterNodes(filter, n.children, [...parents, n.id])
+      ? filterNodes(filter, n.children, [...parents, n.id], stateOverride)
       : INITIAL_FILTERED_VALUE;
 
     return !(filter(n) || filteredChildren.length)
@@ -13,6 +13,10 @@ export const filterNodes = (filter, nodes, parents = []) =>
             ...filtered.nodes,
             {
               ...n,
+              state: {
+                ...(n.state || {}),
+                ...stateOverride
+              },
               children: filteredChildren,
             },
           ],
