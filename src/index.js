07eec9b444ffa92545fe27c6c759a2ef2f62260f
Florante Dela Cruz <fdelacruz@dnanexus.com>
2018-11-13 23:55:07 -0800

    Export filterNodes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Tree from './TreeContainer';
 import * as selectors from './selectors/nodes';
+import {filterNodes} from './selectors/filtering';
 import renderers from './renderers';
 import * as constants from './contants';
 import FilteringContainer from './FilteringContainer';
 
 export default Tree;
-export {selectors, renderers, constants, FilteringContainer};
+export {selectors, renderers, constants, FilteringContainer, filterNodes};
